dbb7f41dd1a15c836d21438998a370695bcca6e3
Huy Nguyen <nqhuy.sgt@gmail.com>
2024-10-02 11:28:41 +0700

    fix env

--- src/config.py
+++ src/config.py
@@ -7,10 +7,10 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     APP_PORT = os.environ.get("APP_PORT") or 5000
-    CHATWORK_API_TOKEN = os.environ.get("CHATWORK_API_TOKEN")
-    OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-    CHATWORK_WEBHOOK_TOKEN = os.environ.get("CHATWORK_WEBHOOK_TOKEN")
-    CHATWORK_WEBHOOK_ID = os.environ.get("CHATWORK_WEBHOOK_ID")
+    CHATWORK_API_TOKEN = os.environ.get("CHATWORK_API_TOKEN", "")
+    OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
+    CHATWORK_WEBHOOK_TOKEN = os.environ.get("CHATWORK_WEBHOOK_TOKEN", "")
+    CHATWORK_WEBHOOK_ID = os.environ.get("CHATWORK_WEBHOOK_ID", "")
 
     # Celery configurations
     CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL") or "redis://redis:6379/0"
