db51b53f940ae9a77e088d134dbe699eadbf5806
Huy Nguyen <nqhuy.sgt@gmail.com>
2024-10-06 18:45:35 +0700

    remove deprecated -v option

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [ "$FLASK_ENV" = "production" ]; then
   # Check if the database file doesn't exist
   if [ ! -f "$SQLITE_DATABASE_NAME" ]; then
     # Attempt to restore the database from the replica
-    litestream restore -v -if-replica-exists -o "$SQLITE_DATABASE_NAME" "$REPLICA_URL"
+    litestream restore -if-replica-exists -o "$SQLITE_DATABASE_NAME" "$REPLICA_URL"
   fi
 
   # Print notice and ASCII art
